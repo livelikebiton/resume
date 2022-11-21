26a2930432ea7447dbbe81fb06207983689d5772
livelikebiton <maayanbiton22@gmail.com>
2022-11-21 12:45:03 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout-area/layout/layout.component';
+import { HeaderComponent } from './components/layout-area/header/header.component';
+import { MainComponent } from './components/layout-area/main/main.component';
+import { FooterComponent } from './components/layout-area/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    LayoutComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LayoutComponent]
 })
 export class AppModule { }
